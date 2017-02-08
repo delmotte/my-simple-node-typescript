7de42858dc2bcd6fe980181efc34770da3127b2c
Jean-Yves DELMOTTE <jean-yves.delmotte@greencom-networks.com>
2017-02-08 17:18:56 +0100

    bug fix after update

--- index.ts
+++ index.ts
@@ -3,14 +3,7 @@ import * as express from 'express';
 
 const app: express.Application = express();
 
-interface AddRequest extends express.Request {
-    params: {
-        x: string;
-        y: string;
-    }
-}
-
-app.get('/add/:x/:y', (req: AddRequest, res: express.Response) => {
+app.get('/add/:x/:y', (req: express.Request, res: express.Response) => {
     if (!req.params.x || !req.params.y) return res.status(400).json(new Error('x & y must be provided'));
     res.status(200).json(add(parseFloat(req.params.x), parseFloat(req.params.y)));
 });
